d11ed97034bb41bcad8c6b1328a56e0475d77fac
Granddz2 <157326500+Granddz2@users.noreply.github.com>
2024-04-02 18:36:20 +0300

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
 const botName = "🎸 MUSIC BOT";
-const rtxText = "RTX";
+const rtxText = "Ayoub DZ or LB";
 const version = "Latest@ v3.6"; 
 const startTime = Date.now();
 
